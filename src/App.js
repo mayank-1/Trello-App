844a8684f8cb8d895a02cbf39c76704050a1249b
Mayank Kumar <mayankkumar@Mayanks-MacBook-Pro.local>
2024-12-16 18:16:34 +0530

    Updated the Trello code as per latest react version

--- src/App.js
+++ src/App.js
@@ -2,102 +2,107 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import Boards from "./components/Boards";
 
-//React Router
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// React Router
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import BoardDetails from "./components/BoardDetails";
 
 class App extends Component {
   state = {
-    title: ""
+    title: "",
   };
 
   render() {
     return (
       <div className="App">
         <Router>
-          <Switch>
-            <Route exact path="/">
-              <div className="navigation">
-                <nav className="navbar shadow-sm navbar-light bg-light">
-                  <span className="navbar-brand mb-0 h1 text-info">
-                    <i className="fab fa-trello"></i> Trello
-                  </span>
-                  <button
-                    className="btn btn-outline-info my-2 my-sm-0"
-                    type="button"
-                    data-toggle="modal"
-                    data-target="#exampleModal"
-                  >
-                    <i className="fas fa-plus"></i> BOARD
-                  </button>
-                </nav>
-                <div
-                  className="modal fade create-new-board"
-                  id="exampleModal"
-                  tabIndex="-1"
-                  role="dialog"
-                  aria-labelledby="exampleModalLabel"
-                  aria-hidden="true"
-                >
-                  <div className="modal-dialog" role="document">
-                    <div className="modal-content">
-                      <div className="modal-header">
-                        <h5 className="modal-title" id="exampleModalLabel">
-                          Create New Board
-                        </h5>
-                        <button
-                          type="button"
-                          className="close"
-                          data-dismiss="modal"
-                          aria-label="Close"
-                        >
-                          <span aria-hidden="true">&times;</span>
-                        </button>
-                      </div>
-                      <div className="modal-body">
-                        <form
-                          onSubmit={e => {
-                            e.preventDefault();
-                            this.props.dispatch({
-                              type: "CREATE_NEW_BOARD",
-                              payload: this.state.title
-                            });
-                            this.setState({
-                              title: ""
-                            });
-                          }}
-                        >
-                          <div className="form-group">
-                            <label htmlFor="boardTitle">Title</label>
-                            <input
-                              id="boardTitle"
-                              className="form-control"
-                              value={this.state.title}
-                              onChange={event =>
-                                this.setState({ title: event.target.value })
-                              }
-                              placeholder="Enter Board Title"
-                              required
-                            />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <div className="navigation">
+                    <nav className="navbar shadow-sm navbar-light bg-light">
+                      <span className="navbar-brand mb-0 h1 text-info">
+                        <i className="fab fa-trello"></i> Trello
+                      </span>
+                      <button
+                        className="btn btn-outline-info my-2 my-sm-0"
+                        type="button"
+                        data-toggle="modal"
+                        data-target="#exampleModal"
+                      >
+                        <i className="fas fa-plus"></i> BOARD
+                      </button>
+                    </nav>
+                    <div
+                      className="modal fade create-new-board"
+                      id="exampleModal"
+                      tabIndex="-1"
+                      role="dialog"
+                      aria-labelledby="exampleModalLabel"
+                      aria-hidden="true"
+                    >
+                      <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                          <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">
+                              Create New Board
+                            </h5>
+                            <button
+                              type="button"
+                              className="close"
+                              data-dismiss="modal"
+                              aria-label="Close"
+                            >
+                              <span aria-hidden="true">&times;</span>
+                            </button>
                           </div>
-                          <button
-                            type="submit"
-                            className="btn form-control btn-info"
-                          >
-                            CREATE
-                          </button>
-                        </form>
+                          <div className="modal-body">
+                            <form
+                              onSubmit={(e) => {
+                                e.preventDefault();
+                                this.props.dispatch({
+                                  type: "CREATE_NEW_BOARD",
+                                  payload: this.state.title,
+                                });
+                                this.setState({
+                                  title: "",
+                                });
+                              }}
+                            >
+                              <div className="form-group">
+                                <label htmlFor="boardTitle">Title</label>
+                                <input
+                                  id="boardTitle"
+                                  className="form-control"
+                                  value={this.state.title}
+                                  onChange={(event) =>
+                                    this.setState({ title: event.target.value })
+                                  }
+                                  placeholder="Enter Board Title"
+                                  required
+                                />
+                              </div>
+                              <button
+                                type="submit"
+                                className="btn form-control btn-info"
+                              >
+                                CREATE
+                              </button>
+                            </form>
+                          </div>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              </div>
-              <div className="allboards p-5">
-                <Boards />
-              </div>
-            </Route>
-            <Route exact path="/board/:id" component={BoardDetails} />
-          </Switch>
+                  <div className="allboards p-5">
+                    <Boards />
+                  </div>
+                </>
+              }
+            />
+            <Route path="/board/:id" element={<BoardDetails />} />
+          </Routes>
         </Router>
       </div>
     );
--- src/components/BoardDetails.js
+++ src/components/BoardDetails.js
@@ -1,104 +1,101 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useParams } from "react-router-dom";
 import { connect } from "react-redux";
 import ListTasks from "./ListTasks";
 import MenuFilter from "./MenuFilter";
 
-class BoardDetails extends Component {
-  state = {
-    taskName: "",
-    currentFilter: "PENDING"
+const BoardDetails = (props) => {
+  const { id } = useParams(); // Extract the board ID from the route params
+  const [taskName, setTaskName] = useState("");
+  const [currentFilter, setCurrentFilter] = useState("PENDING");
+
+  const currentBoard = props.boards.length > 0 ? props.boards[id] : null;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (currentBoard) {
+      props.dispatch({
+        type: "ADD_NEW_TASK_IN_BOARD",
+        payload: {
+          taskName,
+          boardId: id,
+          taskStatus: "PENDING",
+          comments: [],
+        },
+      });
+      setTaskName(""); // Reset the task input field
+    }
   };
 
-  render() {
-    return (
-      <div>
-        <div className="row">
-          <Link to="/">
-            <div className="col-md-1 col-xs-1 m-3 text-info">
-              <i className="fas fa-arrow-left"></i>
+  return (
+    <div>
+      <div className="row">
+        <Link to="/">
+          <div className="col-md-1 col-xs-1 m-3 text-info">
+            <i className="fas fa-arrow-left"></i>
+          </div>
+        </Link>
+        <div className="col-md-6 col-xs-6 mx-auto text-info mt-3">
+          <h4 className="text-center">
+            {currentBoard
+              ? currentBoard.title
+              : "NO BOARD FOUND PLEASE CREATE ONE"}
+          </h4>
+        </div>
+      </div>
+      {currentBoard ? (
+        <div className="row mt-3">
+          <div className="col-md-5 col-xs-8">
+            <div className="col-md-10 col-xs-10 mx-auto">
+              <form onSubmit={handleSubmit}>
+                <h4 className="text-center">Create New Task</h4>
+                <div className="form-group">
+                  <input
+                    id="task-name"
+                    className="form-control"
+                    onChange={(e) => setTaskName(e.target.value)}
+                    type="text"
+                    value={taskName}
+                    placeholder="Enter task name"
+                    required
+                  />
+                </div>
+                <input
+                  type="submit"
+                  className="btn btn-info form-control"
+                  value="ADD TASK"
+                />
+              </form>
             </div>
-          </Link>
-          <div className="col-md-6 col-xs-6 mx-auto text-info mt-3">
-            <h4 className="text-center">
-              {this.props.boards.length > 0
-                ? this.props.boards[this.props.match.params.id].title
-                : "NO BOARD FOUND PLEASE CREATE ONE"}
-            </h4>
+          </div>
+          <div className="col-md-7 col-xs-8 align-self-center mx-auto m-2">
+            <div className="row">
+              <div className="col-md-4 ml-auto p-2 text-center">
+                <MenuFilter filterBy={setCurrentFilter} />
+              </div>
+            </div>
+            <div className="row">
+              <div
+                className="col-md-10 col-xs-8 mx-auto overflow-auto"
+                style={{ height: "600px" }}
+              >
+                <ListTasks filter={currentFilter} />
+              </div>
+            </div>
           </div>
         </div>
-        {this.props.boards.length > 0 ? (
-          <div className="row mt-3">
-            <div className="col-md-5 col-xs-8 ">
-              <div className="col-md-10 col-xs-10 mx-auto">
-                <form
-                  onSubmit={e => {
-                    e.preventDefault();
-                    this.props.dispatch({
-                      type: "ADD_NEW_TASK_IN_BOARD",
-                      payload: {
-                        taskName: this.state.taskName,
-                        boardId: this.props.match.params.id,
-                        taskStatus: "PENDING",
-                        comments: []
-                      }
-                    });
-                    this.setState({
-                      taskName: ""
-                    });
-                  }}
-                >
-                  <h4 className="text-center">Create New Task</h4>
-                  <div className="form-group">
-                    <input
-                      id="task-name"
-                      className="form-control"
-                      onChange={e =>
-                        this.setState({ taskName: e.target.value })
-                      }
-                      type="text"
-                      value={this.state.taskName}
-                      placeholder="Enter task name"
-                      required
-                    />
-                  </div>
-                  <input
-                    type="submit"
-                    className="btn btn-info form-control"
-                    value="ADD TASK"
-                  />
-                </form>
-              </div>
-            </div>
-            <div className="col-md-7 col-xs-8 align-self-center mx-auto m-2">
-              <div className="row">
-                <div className="col-md-4 ml-auto p-2 text-center">
-                  <MenuFilter
-                    filterBy={filter =>
-                      this.setState({ currentFilter: filter })
-                    }
-                  />
-                </div>
-              </div>
-              <div className="row">
-                <div
-                  className="col-md-10 col-xs-8 mx-auto overflow-auto"
-                  style={{ height: "600px" }}
-                >
-                  <ListTasks />
-                </div>
-              </div>
-            </div>
-          </div>
-        ) : (
-          ""
-        )}
-      </div>
-    );
-  }
-}
+      ) : (
+        <p className="text-center text-danger mt-4">
+          Board not found. Please return to the home page and create a new
+          board.
+        </p>
+      )}
+    </div>
+  );
+};
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return state;
 };
 
